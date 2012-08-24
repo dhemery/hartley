9433d8e43fed258ac076d47674a51d896adda2f9
Dale Emery <dale@dhemery.com>
2012-08-23 18:05:07 -0700

    Pollers class with poller factory methods.

---
+++ src/main/java/com/dhemery/polling/Pollers.java
@@ -0,0 +1,28 @@
+package com.dhemery.polling;
+
+import com.dhemery.publishing.Publisher;
+
+/**
+ * Factory methods for common pollers.
+ */
+public class Pollers {
+    /**
+     * Create a simple poller that knows how to poll conditions.
+     */
+    public Poller defaultPoller() {
+        return new EvaluatingPoller(new SimplePollEvaluator());
+    }
+
+    /**
+     * Create a poller that publishes polling events through {@code publisher}.
+     * See {@link com.dhemery.polling.events} for descriptions
+     * of the polling events published by the poller.
+     * @param publisher the publisher through which to publish events
+     * @return a poller that publishes through {@code publisher}
+     */
+    public Poller publishedWith(Publisher publisher) {
+        PollEvaluator simpleEvaluator = new SimplePollEvaluator();
+        PollEvaluator publishingEvaluator = new PublishingPollEvaluator(publisher, simpleEvaluator);
+        return new EvaluatingPoller(publishingEvaluator);
+    }
+}
