472ce73daa5f1e462be1b6308abe9dcd693d8780
Dale Emery <dale@dhemery.com>
2012-09-01 15:56:27 -0700

    SimplePollEvaluator tests

---
+++ src/test/java/com/dhemery/polling/ASimplePollEvaluator.java
@@ -0,0 +1,47 @@
+package com.dhemery.polling;
+
+import com.dhemery.core.Condition;
+import org.jmock.Expectations;
+import org.jmock.auto.Mock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ASimplePollEvaluator {
+    public static final int IGNORED_POLL_COUNT = 0;
+    @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+    @Mock public Condition condition;
+    private PollEvaluator evaluator = new SimplePollEvaluator();
+
+    @Test
+    public void evaluatesTrueIfTheConditionIsSatisfied() {
+        context.checking(new Expectations(){{
+            allowing(condition).isSatisfied(); will(returnValue(true));
+        }});
+
+        boolean result = evaluator.evaluate(condition, IGNORED_POLL_COUNT);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void evaluatesFalseIfTheConditionIsDissatisfied() {
+        context.checking(new Expectations(){{
+            allowing(condition).isSatisfied(); will(returnValue(false));
+        }});
+
+        boolean result = evaluator.evaluate(condition, IGNORED_POLL_COUNT);
+        assertThat(result, is(false));
+    }
+
+    @Test(expected = PollTimeoutException.class)
+    public void failsByThrowingPollTimeoutException() {
+        context.checking(new Expectations(){{
+            ignoring(condition);
+        }});
+
+        evaluator.fail(condition);
+    }
+}
