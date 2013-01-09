486a9e406b12d6409038d33a625b22f9ec994114
Dale Emery <dale@dhemery.com>
2013-01-09 00:27:09 -0800

    Update to latest utility generator

--- src/main/java/com/dhemery/core/Expression.java
+++ src/main/java/com/dhemery/core/Expression.java
@@ -1,14 +1,16 @@
 package com.dhemery.core;
 
-import com.dhemery.factory.FactoryAnnotation;
+import com.dhemery.generator.Generate;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@FactoryAnnotation("com.dhemery.expressing.Expressions")
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
+@Generate(
+        className="com.dhemery.expressing.Expressions",
+        description="Factory methods for operators, features, and other expression items.")
 public @interface Expression {
 }
