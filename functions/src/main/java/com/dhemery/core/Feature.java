7fed153aef42b4aca3a1234138996a43ba53183a
Dale Emery <dale@dhemery.com>
2015-04-13 16:29:47 -0700

    Feature extends Function

--- functions/src/main/java/com/dhemery/core/Feature.java
+++ functions/src/main/java/com/dhemery/core/Feature.java
@@ -2,14 +2,32 @@ package com.dhemery.core;
 
 import org.hamcrest.SelfDescribing;
 
+import java.util.function.Function;
+
 /**
- * Retrieves the value of a feature for a subject.
- * @param <S> The type of subject that has the feature
- * @param <F> The type of feature
+ * A {@link SelfDescribing SelfDescribing} {@link Function Function}.
+ * <p>
+ * This is an obsolete interface,
+ * designed before Java 8 introduced functional interfaces.
+ * For new self-describing functions,
+ * implement both {@code FunctionFunction}
+ * and {@code SelfDescribing SelfDescribing}.
+ * If you do not need the function to describe itself,
+ * prefer {@link Function Function}.
+ * </p>
  */
-public interface Feature<S, F>  extends SelfDescribing {
+public interface Feature<T, R>  extends Function<T,R>, SelfDescribing{
 	/**
      * Return the subject's current value for the feature.
 	 */
-	F of(S subject);
+	R of(T t);
+
+	/**
+	 * Delegates to {@link #of(T)}.
+	 * <p>
+	 * NOTE: Do not override this method.
+	 * If you need this method to do something else,
+	 * implement {@link Function} instead.
+	 */
+	default R apply(T t) { return of(t); }
 }
